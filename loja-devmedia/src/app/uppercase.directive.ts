df31b7998c84ac021c77d5fe45047b17370878e4
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-07-15 19:22:37 -0300

    10. Criando a própria diretiva

---
+++ loja-devmedia/src/app/uppercase.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostListener, ElementRef } from "@angular/core";
+
+@Directive({
+    selector: '[upperCase]'
+})
+export class UppercaseDirective {
+    constructor(
+        private el: ElementRef
+    ) {}
+
+    @HostListener('keyup') keyup() {
+        this.el.nativeElement.value = this.el.nativeElement.value.toUpperCase();
+    }
+}
